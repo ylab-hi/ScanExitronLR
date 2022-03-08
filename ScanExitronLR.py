08bb06f93824e056b180c1b319de19cea178e34d
Josh Fry <fryxx094@umn.edu>
2022-03-08 17:32:47 -0600

    removed ChrM from search space, it just added extra computed time

--- ScanExitronLR.py
+++ ScanExitronLR.py
@@ -927,7 +927,7 @@ def main(tmp_path):
         'chr6', 'chr7', 'chr8', 'chr9', 'chr10',
         'chr11','chr12', 'chr13', 'chr14', 'chr15',
         'chr16','chr17', 'chr18', 'chr19', 'chr20',
-        'chr21', 'chr22', 'chrX', 'chrY', 'chrM'] if not args.arabidopsis else \
+        'chr21', 'chr22', 'chrX', 'chrY'] if not args.arabidopsis else \
         ['Chr1', 'Chr2', 'Chr3', 'Chr4',
          'Chr5', 'ChrM', 'Chrchloroplast']
 
